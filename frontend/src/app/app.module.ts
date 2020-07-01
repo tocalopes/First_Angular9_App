806c760f93c883be65f18dccf70cadb1b3a08c20
Gabriel <gabriellopes112009@gmail.com>
2020-07-01 20:10:39 -0300

    Beggining of Home implementation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+
+import {MatCardModule} from '@angular/material/card'; 
 
 
 @NgModule({
@@ -17,7 +20,8 @@ import {MatListModule} from '@angular/material/list';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,8 +29,10 @@ import {MatListModule} from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
